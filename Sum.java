a204b7ff6e70742fa0c7b6226c3befd337025df5
moegammadallie <47336541+moegammadallie@users.noreply.github.com>
2019-02-12 21:30:40 +0200

    Added something
    I've included another variable (num3) into the equation.

--- Sum.java
+++ Sum.java
@@ -1,10 +1,11 @@
-public class Sum {
+cpublic class Sum {
 
     /*This is just up because it needs to be...I'm not this stupid*/
 
     public static void Main(String[] args){
         int num1 = 12584;
         int num2 = 248735;
-        System.out.println("The sum of 12584 and 248135 is" + (num1 + num2));
+        int num3 = 911;//Moegammad's edit
+        System.out.println("The sum of 12584 and 248135 and 911 is" + (num1 + num2 + num3));
     }
 }
